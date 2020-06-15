0dbac7763475f6ef5e4de1a949ad61fd79121e1c
rodrigo.b.silva <rodrigo.barbosa@fcamara.com.br>
2020-06-14 23:18:53 -0300

    Schedule appointments e upload avatar

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,19 +1,31 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import User from '../app/models/User';
+import File from '../app/models/File';
+import Appointments from '../app/models/Appointments';
 
 import databaseConfig from '../config/database';
 
-const models = [User];
+const models = [User, File, Appointments];
 
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
     this.connection = new Sequelize(databaseConfig);
-    models.map((model) => model.init(this.connection));
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo(){
+    this.mongoConnection = mongoose.connect('mongodb://localhost:27017/gobarber', {
+      useNewUrlParser: true, useFindAndModify: true
+    });
   }
 }
 
