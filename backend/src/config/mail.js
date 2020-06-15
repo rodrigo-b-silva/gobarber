0b3649c721754893a92b8ed2c42d7d64a7629001
rodrigo.b.silva <rodrigo.barbosa@fcamara.com.br>
2020-06-14 23:21:44 -0300

    Template de email e fila de jobs

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  secure: false,
+  auth: {
+    user: "04b07428de0a3d",
+    pass: "ac9ee62befb2a4"
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber>',
+  }
+}
