982755d10c1c7ec68a0b33d388190a41706764ec
rodrigo.b.silva <rodrigo.barbosa@fcamara.com.br>
2020-06-21 19:23:25 -0300

    Adição .env

--- backend/src/config/auth.js
+++ backend/src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: 'jwt-secret-gobarber',
+  secret: process.env.APP_SECRET,
   expiresIn: '7d',
 };
---
+++ backend/src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://e90f455b5e7e43e78f5fafaba0ad6c7d@o407931.ingest.sentry.io/5277894'
+}
